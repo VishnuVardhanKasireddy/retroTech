62b2b23fa789b9f59be782cd78f43d689fb169bc
VishnuVardhanKasireddy <kasireddyvishnuvardhanreddy18@gmail.com>
2026-07-18 20:48:33 +0530

    feat : intialize react with vite

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express=require("express")
 const router = require("./routes/productRoutes.js")
 const {errorHandler}=require("./middleware/errorHandler.js")
 const app=express()
+const cors = require("cors")
 
+app.use(cors)
 app.use(express.json());
 
 app.get("/health",(req,res)=>{
